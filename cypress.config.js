fceb6f26ac78ee687668bf556d4f56cf923c1ce0
Tiago rosa da costa <tiagorosadacost@gmail.com>
2024-06-03 22:03:51 -0300

    changes to resolve pipeline was break

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true
   },
 });
